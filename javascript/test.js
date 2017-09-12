5858440d1dd433014942d71fee0789b90e260665
strawji02 <strawji02@naver.com>
2017-09-12 23:39:19 +0900

    javascript

---
+++ javascript/test.js
@@ -0,0 +1,7 @@
+var a = document.getElementById("b");
+function yellow_btn_click(){
+    a.style.backgroundColor = "yellow";
+}
+function white_btn_click(){
+    a.style.backgroundColor = "white";
+}
