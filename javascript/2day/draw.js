d61501ccb487316125da88698719a21c9b72ec98
strawji02 <strawji02@naver.com>
2017-09-14 23:50:52 +0900

    ㅁㄴㅇㄹ

--- javascript/2day/draw.js
+++ javascript/2day/draw.js
@@ -51,46 +51,46 @@ image.addEventListener('load', function() {
 
 //방법 2. 키보드 이벤트 + 배열 사용
 
-// let downKeys = {};
+let downKeys = {};
 
-// window.addEventListener('keydown', onKeyDown);
-// window.addEventListener('keyup', onKeyUp);
+window.addEventListener('keydown', onKeyDown);
+window.addEventListener('keyup', onKeyUp);
 
-// function onKeyDown(event){
-//     downKeys[event.code] = true;
-// }
+function onKeyDown(event){
+    downKeys[event.code] = true;
+}
 
-// function onKeyUp(event){
-//     downKeys[event.code]=false;
-// }
+function onKeyUp(event){
+    downKeys[event.code]=false;
+}
 
-// window.requestAnimationFrame(run);
+window.requestAnimationFrame(run);
 
-// function run(){
-//     // context.fillStyle = 'white';
-//     // context.fillRect(0, 0, canvas.width, canvas.height);
+function run(){
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-//     context.drawImage(image,x,y);
+    context.drawImage(image,x,y);
     
-//     if(downKeys['ArrowLeft'])
-//         x -= 10;
-//     if(downKeys['ArrowRight'])
-//         x += 10;
-//     if(downKeys['ArrowUp'])
-//         y -= 10;
-//     if(downKeys['ArrowDown'])
-//         y += 10;
-//     if(x >= 800)
-//         x = 0;
-//     if(y >= 800)
-//         y = 0;
-//     if(x < 0)
-//         x = 800;
-//     if (y < 0)
-//         y = 800;
-//     window.requestAnimationFrame(run);
-    
-// }
+    if(downKeys['ArrowLeft'])
+        x -= 10;
+    if(downKeys['ArrowRight'])
+        x += 10;
+    if(downKeys['ArrowUp'])
+        y -= 10;
+    if(downKeys['ArrowDown'])
+        y += 10;
+    if(x >= 800)
+        x = 0;
+    if(y >= 800)
+        y = 0;
+    if(x < 0)
+        x = 800;
+    if (y < 0)
+        y = 800;
+    window.requestAnimationFrame(run);
+}
+
 // function getMousePosition(event){
 //     const rect=canvas.getBoundingClientRect();
 //     return{
@@ -113,41 +113,3 @@ image.addEventListener('load', function() {
 //     // if((can.x >= x && can.x <= x + 100) && (can.y >= y && can.y <= y+100)){
 //     moveImage(event);
 // })
-var img_L = 10;
-var img_T = 20;
-var targetObj;
-
-function getLeft(o){
-return parseInt(o.style.left.replace('px', ''));
-}
-function getTop(o){
-return parseInt(o.style.top.replace('px', ''));
-}
-
-// 이미지를 움직이는 함수입니다 움직였던 위치만큼 처음 이미지가 있던 좌표를 더해줍니다 최종 위치입니다
-function moveDrag(e){
-var e_obj = window.event? window.event : e;
-var dmvx = parseInt(e_obj.clientX + img_L);
-var dmvy = parseInt(e_obj.clientY + img_T);
-targetObj.style.left = dmvx +"px";
-targetObj.style.top = dmvy +"px";
-return false;
-}
-
-// 드래그를 시작하는 함수 입니다. 움직였던 좌표에서 처음 드래그를 시작했던 좌표를 빼줍니다. 움직인 좌표를 나타내줍니다
-function startDrag(e, obj){
-targetObj = obj;
-var e_obj = window.event? window.event : e;
-img_L = getLeft(obj) - e_obj.clientX;
-img_T = getTop(obj) - e_obj.clientY;
-
-document.onmousemove = moveDrag;
-document.onmouseup = stopDrag;
-if(e_obj.preventDefault)e_obj.preventDefault();
-}
-
-// 드래그를 멈추는 함수 입니다
-function stopDrag(){
-document.onmousemove = null;
-document.onmouseup = null;
-}
